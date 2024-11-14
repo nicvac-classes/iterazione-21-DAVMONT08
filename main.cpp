0781c3b98164fec2f39b024abe28fc545037957f
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-11-14 17:55:49 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+              int p,pa,pb;
+              p=0;
+              pa=0;
+              pb=0;
+              while(p<30)
+              {
+                 cout<<"quanti adulti sono saliti?"<<endl;
+                 cin>> pa;
+                 cout<<" quanti bambini sono saliti?"<<endl;
+                 cin>> pb;
+                 p=p+pa;
+                 p=p+(pb/2);
+              }
+              if(p>30)
+              {
+                cout<<"bisogna liberare " << p-30 <<endl;
+              } 
+              else
+              {
+                 cout<<" la giostra è al completo"<<endl;
+              }
+     
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
